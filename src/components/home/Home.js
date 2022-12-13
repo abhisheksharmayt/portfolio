fcb685c8ed17f69651b8f49facced31753fd8703
Abhishek Sharma <ohsharmaabhi03@gmail.com>
2022-12-13 17:37:59 +0530

    add: github streak chart

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Navbar from '../navbar/Navbar'
 import Social from '../social/social'
 import profile from '../../images/profile-picture.jpeg'
@@ -9,8 +9,8 @@ import { messages } from './data'
 const Home = () => {
     const [showPopup, setShowPopup] = useState(false);
     const [index, setIndex] = useState(0);
-    const handleImgClick = ()=>{
-        if(index<5) setIndex(index+1);
+    const handleImgClick = () => {
+        if (index < 5) setIndex(index + 1);
     }
     return (
         <>
@@ -18,15 +18,19 @@ const Home = () => {
             <main>
                 <section className='intro-section'>
                     <div className='profile-img-container' onClick={handleImgClick}
-                    onMouseEnter={()=>setShowPopup(true)} onMouseLeave={()=>setShowPopup(false)}>
+                        onMouseEnter={() => setShowPopup(true)} onMouseLeave={() => setShowPopup(false)}>
                         <img src={profile} alt="profile picture" className='profile-pic' />
-                        {showPopup && 
-                        <div className='img-popup'>
-                            <p>{messages[index]}</p>
-                        </div>}
+                        {showPopup &&
+                            <div className='img-popup'>
+                                <p>{messages[index]}</p>
+                            </div>}
                     </div>
                     <h1 className='intro'>Hi, I am <span className='name'>Abhishek Sharma</span></h1>
                     <h4 className='info'>Frontend web developer with a passion for aesthetic interfaces</h4>
+                    <h2 className='github-title'>My Contributions</h2>
+                    <div className='github-div'>
+                        <img className="github-chart-img" src="https://ghchart.rshah.org/abhisheksharmayt" alt="github chart" />
+                    </div>
                     <div className="skills-div">
                         {
                             skills.map((icon) => {
