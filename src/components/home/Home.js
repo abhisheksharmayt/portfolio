03240623c16c3c094dcc3baabf07550a66d0f618
Abhishek Sharma <abhisheksharmayt2@gmail.com>
2024-03-24 03:16:46 +0530

    fix typo

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
                     </div>
                     <h1 className='intro'>Hi, I am <span className='name'>Abhishek Sharma</span></h1>
                     <h4 className='info'>Frontend web developer with a passion for aesthetic interfaces</h4>
-                    <h2 className='github-title'>My Contributions</h2>
+                    {/* <h2 className='github-title'>My Contributions</h2> */}
                     {/* <div className='github-div'>
                         <img className="github-chart-img" src="https://ghchart.rshah.org/abhisheksharmayt" alt="github chart" />
                     </div> */}
